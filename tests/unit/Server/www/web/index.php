59bea6cbc29035211f6d471ccf426a2fda46d00a
Juan Francisco Calderon Zumba <juan@sntc.no>
2015-06-09 13:45:14 +0200

    US49; Unit tests refactoring to think ahead for Driver integration tests

--- tests/unit/Server/www/web/index.php
+++ tests/unit/Server/www/web/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../../../../vendor/autoload.php';
+require_once __DIR__ . '/../../../../../vendor/autoload.php';
 
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
