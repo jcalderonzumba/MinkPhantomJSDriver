78175619d1816b8215bebea6cd9ae90a1fd177d1
Jérôme Vieilledent <lolautruche@gmail.com>
2016-08-03 14:21:17 +0200

    Fix wait timeout
    Per `Behat\Mink\Driver\DriverInterface::wait()`, `$timeout` must be expressed in milliseconds.
    However, in `JavaScriptTrait::wait()`, `$end` is wrongly computed, dividing the milliseconds value by `1000.0` instead of multiplying it.
    
    Because of this, you can get unexpected issues, like elements not present yet in the DOM, even if you use `$session->wait()`.

--- src/JavascriptTrait.php
+++ src/JavascriptTrait.php
@@ -37,7 +37,7 @@ trait JavascriptTrait {
    */
   public function wait($timeout, $condition) {
     $start = microtime(true);
-    $end = $start + $timeout / 1000.0;
+    $end = $start + $timeout * 1000.0;
     do {
       $result = $this->browser->evaluate($condition);
       usleep(100000);
